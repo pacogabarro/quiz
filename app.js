8fdff4eae774a4ec3cd816fd4556c3c681bc45df
pacogabarro <pacogabarro@gabarrofuentes.es>
2015-07-01 23:07:04 +0200

    Primera página y favicon

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Importar paquetes con middlewares
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,26 +6,31 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//Importar enrutadores
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
+//Crear aplicación
 var app = express();
 
-// view engine setup
+// Instalar generador de vistas ejs
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+//Instalar middlewares
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Instalar enrutadores asociando rutas a sus gestores
 app.use('/', routes);
-app.use('/users', users);
 
+
+//Gestión de errores
+
+//Error para una ruta no definida
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
@@ -32,9 +38,7 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-// error handlers
-
-// development error handler
+// Gestión de errores en desarrollo
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
@@ -46,7 +50,7 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
+// Gestión de errores en producción
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -57,4 +61,5 @@ app.use(function(err, req, res, next) {
 });
 
 
+//Exportar app para módulo de arranque
 module.exports = app;
