c87b8717c87dd085813b08ff6a9fccc8d0f6d374
pacogabarro <pacogabarro@gabarrofuentes.es>
2015-07-04 20:31:25 +0200

    Créditos_sin_foto

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,20 @@
 
 //GET /quizes/question
 exports.question=function(req, res){
-    res.render('quizes/question', {pregunta='Capital de Italia'});
+    res.render('quizes/question', {pregunta: 'Capital de Italia'});
 };
 
 
 //GET /quizes/answer
 exports.answer=function(req, res){
   if (req.query.respuesta === 'Roma'){
-    res.render('quizes/answer', {respuesta = 'Correcto'});
+    res.render('quizes/answer', {respuesta: 'Correcto'});
   }else {
-    res.render('quizes/answer', {respuesta = 'Incorrecto'});
+    res.render('quizes/answer', {respuesta: 'Incorrecto'});
   }
+};
+
+//GET /author
+exports.author=function(req, res){
+  res.render('author', {autor: 'Paco Gabarro'});
 };
