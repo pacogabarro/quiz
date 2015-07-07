d274ae0c2fe844c1c358c1908eed8b0279949cde
pacogabarro <pacogabarro@gabarrofuentes.es>
2015-07-07 19:02:24 +0200

    Despliegue en Heroku con lista y autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -15,7 +15,7 @@ exports.load = function(req, res, next, quizId) {
 //GET /quizes
 exports.index = function(req, res) {
   models.Quiz.findAll().then(function(quizes){
-    res.render('quizes/index.ejs', {quizes; quizes});
+    res.render('quizes/index.ejs', {quizes: quizes});
   })
 };
 
