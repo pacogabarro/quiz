c9922212c28530a05aa01379e1caad0ffc5ca39e
pacogabarro <pacogabarro@gabarrofuentes.es>
2015-07-07 06:36:00 +0200

    Desplieque en Heroku con Postgres

---
+++ models/models.js
@@ -0,0 +1,45 @@
+var path = require('path');
+
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/DATABASE_URL
+// SQLite DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user = (url[2]||null);
+var pwd = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect = (url[1]||null);
+var port = (url[5]||null);
+var host = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
+//Cargar modelo ORM
+var Sequelize = require('sequelize');
+
+//Usar BBDD SQlite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd,
+      {dialect: protocol,
+       protocol: protocol,
+       port: port,
+       host: host,
+       storage: storage,  // Solo SQLite (.env)
+       omitNull: true
+       }   // Solo Postgres
+);
+
+//Importar la definición de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+//Exportar definición de la tabla Quiz
+exports.Quiz = Quiz;
+
+//Crear e inicializar la tabla en DB
+sequelize.sync().success(function() {
+  Quiz.count().success(function (count) {
+    if(count === 0) {
+      Quiz.create( { pregunta: 'Capital de Italia',
+                     respuesta: 'Roma'
+                    })
+      .success(function() {console.log('Base de datos inicializada')});
+    };
+  });
+});
