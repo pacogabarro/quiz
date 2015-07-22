37ae79eab3a2941923b04059c84bf6feb4336793
pacogabarro <pacogabarro@gabarrofuentes.es>
2015-07-22 20:06:59 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -19,13 +19,13 @@ exports.index = function(req, res) {
       req.query.search="";
   };
   models.Quiz.findAll({where:["pregunta LIKE ?", "%" + req.query.search + "%"]}).then(function(quizes){
-    res.render('quizes/index.ejs', {quizes: quizes});
+    res.render('quizes/index.ejs', {quizes: quizes, errors: []});
   })
 };
 
 //GET /quizes/:id
 exports.show = function(req, res) {
-  res.render('quizes/show', {quiz: req.quiz});
+  res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 
@@ -35,7 +35,10 @@ exports.answer=function(req, res){
   if (req.query.respuesta === req.quiz.respuesta){
     resultado = 'Correcto';
   }
-  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+  res.render('quizes/answer', {quiz: req.quiz,
+                                respuesta: resultado,
+                                errors: []}
+  );
 };
 
 
@@ -44,17 +47,27 @@ exports.new = function(req, res){
   var quiz = models.Quiz.build(  // Crea objeto quiz
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 
 // POST /quizes/create
 exports.create = function(req, res){
   var quiz = models.Quiz.build(req.body.quiz);
-  // guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then (function(){
-    res.redirect('/quizes'); // redirección HTTP (URL relativo) a la lista de preguntas
-  })
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+          // guarda en DB los campos pregunta y respuesta de quiz
+          quiz
+          .save({fields: ["pregunta", "respuesta"]})
+          .then (function(){
+          res.redirect('/quizes')}) // redirección HTTP (URL relativo) a la lista de preguntas
+        }
+    });
 };
 
 
